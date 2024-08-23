593858d9f1d9722aed9039827f7f4031adc22c5b
Sarfaraz Unar <sarfarazunarr@gmail.com>
2024-08-23 18:20:17 +0500

    TEMPORARY EMAIL SERVER ADDED

--- app/lib/sendEmail.js
+++ app/lib/sendEmail.js
@@ -1,26 +1,17 @@
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD
-  }
-});
-
-const sendEmail = (to, subject, text) => {
-    const mailOptions = {
-        from: process.env.EMAIL,
-        to,
-        subject,
-        text
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log('Error occurred:', error);
-        } else {
-            console.log('Email sent:', info.response);
+import axios from 'axios'
+const sendEmail = async (to, subject, text) => {
+    try {
+        const data = {
+            to: to,
+            subject: subject,
+            text: text
         }
-    });
+        const response = await axios.post(process.env.EMAIL_SERVER, data);
+        if (response.status == 200) {
+            return true;
+        }
+    } catch (error) {
+        return false;   
+    }
 }
 module.exports = sendEmail;
