5789eecc83d895dd20190fa111a765b7520473d0
Sarfaraz Unar <sarfarazunarr@gmail.com>
2024-08-28 12:50:43 +0500

    config file reverted

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'flowbite.s3.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pagedone.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-      },
-    ],
+    domains: ['images.unsplash.com', "flowbite.s3.amazonaws.com", "pagedone.io", "placehold.co", "i.imgur.com"],
   },
 };
 
