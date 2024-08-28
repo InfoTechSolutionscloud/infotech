6e56090af1349a85748214d3ce177365d135e11c
Sarfaraz Unar <130893009+sarfarazunarr@users.noreply.github.com>
2024-08-28 15:55:46 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', "flowbite.s3.amazonaws.com", "pagedone.io", "placehold.co", "i.imgur.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
 };
+}
 
 export default nextConfig;
