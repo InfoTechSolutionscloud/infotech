8da0c3208c52c52ec952294ac21cbb7aee5df9de
InfoTech <infotechcompany85@gmail.com>
2024-12-05 15:09:16 +0500

    Update page.js

--- app/team/page.js
+++ app/team/page.js
@@ -85,7 +85,7 @@ const page = () => {
               </button>
                    <button
                 className={`${
-                  show == "Portfolio"
+                  show == "ourservices"
                     ? "bg-secondary-700 text-white"
                     : "bg-gray-800"
                 } p-3 rounded-md text-white luto font-normal hover:border-secondary-500 hover:border border border-transparent transition-colors duration-150 disabled:bg-gray-600 disabled:text-gray-800 disabled:cursor-not-allowed`}
