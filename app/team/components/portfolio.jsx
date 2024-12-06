6b0e7a6730f1346ffdd00a77355c91145132785a
InfoTech <infotechcompany85@gmail.com>
2024-12-06 20:02:05 +0500

    Update portfolio.jsx

--- app/team/components/portfolio.jsx
+++ app/team/components/portfolio.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
-const ManageServices = () => {
-  const [services, setServices] = useState([]);
+const ManagePortfolio = () => {
+  const [portfolio, setPortfolio] = useState([]);
   const [refresh, setRefresh] = useState(0);
  
   const token = localStorage.getItem('token');
@@ -12,13 +12,13 @@ const ManageServices = () => {
   useEffect(() => {
     const getdata = async () => {
       try {
-        const res = await axios.get("/api/service/all", {
+        const res = await axios.get("/api/portfolio/all", {
           headers: {
             Authorization: `Bearer ${token}`
           }
         });
         if (res.status == 200) {
-          setServices(res.data.services);
+          setPortfolio(res.data.portfolio);
         }
       } catch (error) {
         setMessage(error.response.data.message)
@@ -28,7 +28,7 @@ const ManageServices = () => {
     getdata();
   }, [refresh])
 
-  const deleteService = async (id) => {
+  const deletePortfolio = async (id) => {
     try {
       const res = await axios.delete(`${origin}?id=${id}`, {
         headers: {
@@ -48,23 +48,23 @@ const ManageServices = () => {
     <>
       <div className="container mx-auto w-full p-4">
         <div className="flex items-center justify-between mb-4">
-          <h1 className="text-2xl text-white font-bold">Services</h1>
-          <Link href={'/team/create-service'} className="bg-secondary-600 hover:bg-secondary-900 text-white raleway py-2 px-4 rounded">
+          <h1 className="text-2xl text-white font-bold">Portfolio</h1>
+          <Link href={'/team/create-portfolio'} className="bg-secondary-600 hover:bg-secondary-900 text-white raleway py-2 px-4 rounded">
             Create New Portfolio
           </Link>
         </div>
-        {!services && (
+        {!portfolio && (
           <div className="flex justify-center items-center">
             <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white">Loading...</div>
           </div>
         )}
-        {services && services.length == 0 && (
+        {portfolio && portfolio.length == 0 && (
           <div className="flex justify-center items-center">
-            <p className='text-center text-2xl text-gray-400 py-5'>No Services Found!</p>
+            <p className='text-center text-2xl text-gray-400 py-5'>No Portfolio Found!</p>
           </div>
         )}
 
-        {services && services.length > 0 && (<div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+        {portfolio && portfolio.length > 0 && (<div className="overflow-x-auto relative shadow-md sm:rounded-lg">
           <table className="w-full text-sm text-left text-gray-400">
             <thead className="text-xs uppercase bg-gray-700 text-gray-400">
               <tr>
@@ -83,7 +83,7 @@ const ManageServices = () => {
               </tr>
             </thead>
             <tbody>
-              {services.map((item, index) => {
+              {portfolio.map((item, index) => {
                 return (
                   <tr key={index} className=" border-b border-gray-700 hover:bg-gray-600">
                     <td className="px-6 py-4">
@@ -96,13 +96,13 @@ const ManageServices = () => {
                       {item.short_description}
                     </td>
                     <td className="px-6 py-4">
-                      <Link href={`/services/${item.slug}`} className="font-bold text-blue-500 hover:underline mr-2">
+                      <Link href={`/portfolio/${item.slug}`} className="font-bold text-blue-500 hover:underline mr-2">
                         View
                       </Link>
-                      <Link href={`/team/update-service/${item.slug}`} className="font-bold text-blue-500 hover:underline mr-2">
+                      <Link href={`/team/update-portfolio/${item.slug}`} className="font-bold text-blue-500 hover:underline mr-2">
                         Update
                       </Link>
-                      <button className="font-bold text-red-500 hover:underline mr-2" onClick={() => deleteService(item._id)}>
+                      <button className="font-bold text-red-500 hover:underline mr-2" onClick={() => deletePortfolio(item._id)}>
                         Delete
                       </button>
                     </td>
@@ -119,4 +119,4 @@ const ManageServices = () => {
   )
 }
 
-export default ManageServices;
+export default ManagePortfolio;
