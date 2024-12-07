f3fb3f8b9fceabd5bfe8141af96b79319ba15ae1
InfoTech <infotechcompany85@gmail.com>
2024-12-07 16:38:30 +0500

    Update route.js

--- app/api/portfolio/all/route.js
+++ app/api/portfolio/all/route.js
@@ -1,1 +1,14 @@
+import dbConnect from "@/app/lib/mongoose";
+import ourportfolio from "@/app/models/Ourportfolio";
 
+export const GET = async (request) => {
+    try {
+        await dbConnect();
+        const findSPortfolio = await ourportfolio.find({});
+        
+        return new Response(JSON.stringify({portfolio: findPortfolio}), {status: 200});
+    } catch (error) {
+        console.log(error);
+        return new Response(JSON.stringify({error: error}), {status: 500})
+    }
+}
