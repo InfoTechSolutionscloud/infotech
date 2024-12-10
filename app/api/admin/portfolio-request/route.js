f025bc6b45f1cc4acc4f493004de49f501c71a14
InfoTech <infotechcompany85@gmail.com>
2024-12-10 13:04:22 +0500

    Update route.js

--- app/api/admin/portfolio-request/route.js
+++ app/api/admin/portfolio-request/route.js
@@ -1,1 +1,27 @@
+import authMiddleware from "@/app/lib/authMiddleware";
+import dbConnect from "@/app/lib/mongoose";
+import Portfolio from "@/app/models/Portfolio ";
 
+
+async function getit(request) {
+    await dbConnect();
+    const user = request.user;
+    if(user.role !== 'admin') return new Response(JSON.stringify({message: "You are not admin"}), {status: 403});
+    const data = await Portfolio.find()
+    if(!data) return new Response(JSON.stringify({message: "No Data Found"}), {status: 404})
+    return new Response(JSON.stringify({data: data}), {status: 200})
+}
+
+async function deleteit(request){
+    await dbConnect();
+
+    const user = request.user;
+    if(user.role !== 'admin') return new Response(JSON.stringify({message: "You are not admin"}), {status: 403});
+    const id = request.nextUrl.searchParams.get("id");
+    const deletedata = await Portfolio.findByIdAndDelete(id)
+    if(!deletedata) return new Response(JSON.stringify({message: "Failed to Delete"}),{status: 500})
+    return new Response(JSON.stringify({message: "Deleted Successfully!"}), {status: 200})
+}
+
+export const GET = authMiddleware(getit)
+export const DELETE = authMiddleware(deleteit)
