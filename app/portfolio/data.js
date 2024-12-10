a59711af4ea49b31308c5beaa97091ff66f56168
InfoTech <infotechcompany85@gmail.com>
2024-12-10 12:22:08 +0500

    Delete app/portfolio/data.js

--- app/portfolio/data.js
+++
@@ -1,8 +0,0 @@
-const data = [
-    {img: "https://i.imgur.com/GLzVfvd.png", title: "Tution Portal Banner", short_description: "A flyer design for getting started with Tution Portal", category: "Graphic Design"},
-    {img: "https://i.imgur.com/xkn5Wq6.jpeg", title: "JavaScript AI", short_description: "A best AI based JavaScript Chatbot", category: "Web Development, Python"},
-    {img: "https://i.imgur.com/VDz43du.jpeg", title: "Full Form GPT", short_description: "A best AI based tool for generating full forms", category: "Python, GPT Api"},
-    {img: "https://i.imgur.com/lMRuMjX.jpeg", title: "Livelap Ecommerce Website", short_description: "A best ecommerce laptop selling website with Stripe", category: "Web Development"},
-    {img: "https://i.imgur.com/MZvi5Zp.jpeg", title: "Quizly - Quiz Website", short_description: "A best quiz generation website for teachers and students", category: "Web Development"}
-]
-export default data;
