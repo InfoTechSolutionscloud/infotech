3e6ec1dbc0c036d723a2ee13e4b3748f654163d7
InfoTech <infotechcompany85@gmail.com>
2024-12-10 13:07:48 +0500

    Update Portfolio.js

--- app/models/Portfolio.js
+++ app/models/Portfolio.js
@@ -1,1 +1,32 @@
+import mongoose from "mongoose";
 
+const portfolioSchema = new mongoose.Schema({
+    clientName: {
+        type: String,
+        required: true
+    },
+    clientEmail: {
+        type: String,
+        required: true
+    },
+    clientPhone: {
+        type: String,
+        required: true
+    },
+    Portfolio: {
+        type: String,
+        required: true
+    },
+    details: {
+        type: String,
+        required: true
+    },
+    budget: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+export default mongoose.models.Portfolio || mongoose.model("Portfolio", portfolioSchema);
