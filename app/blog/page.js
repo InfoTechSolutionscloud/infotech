755d68c6500a259d27a94c3eef9b0126e2be417f
InfoTech <infotechcompany85@gmail.com>
2024-12-16 14:42:41 +0500

    Update page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react'
-import Blog from '../components/Blogs'
+import Blogs from '../components/Blogs'
 import CustomHead from '../components/CustomHead';
 
 const page = () => {
@@ -11,7 +11,7 @@ const page = () => {
     <>
       <CustomHead title={"Our Blog - Infotech"} description={"Stay updated with the latest insights, trends, and news from the world of technology. Our blog features articles on web development, digital marketing, IT consulting, software development, and more. Learn from our experiences, success stories, and industry expertise."} keywords={"web development company, digital marketing agency, IT consulting services, software development company, technology solutions, innovative solutions, real results, web design, e-commerce solutions, mobile app development, SEO services, PPC advertising, software testing, IT consulting, web application development, custom software development"} fullUrl={fullUrl} />
       <div className='bg-black'>
-        <Blog title='Our' />
+        <Blogs title='Our' />
       </div>
     </>
   )
