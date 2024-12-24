394fc6e4f5e98d904016d52a09e3ba2b13060190
InfoTech <infotechcompany85@gmail.com>
2024-12-24 11:58:50 +0500

    Update route.js

--- app/api/portfolio/route.js
+++ app/api/portfolio/route.js
@@ -16,15 +16,15 @@ const createPortfolio = async(request) => {
     await dbConnect()
 
     const user = request.user;
-    if(user.role !== 'admin') return new Response(JSON.stringify({message: "Only admin can add portfolio"}), {status: 403})
+    if(user.role !== 'admin') return new Response(JSON.stringify({message: "Only admin can add Portfolio"}), {status: 403})
 
-    const {image, pdf, title, short_description, slug} = await request.json();
-    if(!title, !short_description) return new Response(JSON.stringify({message: "All Fields are required!"}), {status: 401})
+    const {image, title, short_description, description, slug} = await request.json();
+    if(!title, !short_description, !description) return new Response(JSON.stringify({message: "All Fields are required!"}), {status: 401})
     let newslug;
     if(!slug) newslug = title.toLowerCase().replace(" ", "-"); else newslug = slug;
 
     const createportfolio = await new Ourportfolio({
-        image, pdf, title, short_description, slug: newslug
+        image, title, short_description, description, slug: newslug
     });
     try {
         await createportfolio.save(); // Add await to ensure the promise is resolved
@@ -39,10 +39,10 @@ const updatePortfolio = async (request) => {
 
     const user = request.user;
     if(user.role !== 'admin') return new Response(JSON.stringify({message: "Only admin can add portfolio"}), {status: 401})
-    const {_id, image, pdf, title, short_description, slug } = await request.json();
-    if(!_id, !title, !short_description, !slug) return new Response(JSON.stringify({message: "All Feilds are required"}), {status: 403})
+    const {_id, image, title, short_description, description, slug } = await request.json();
+    if(!_id, !title, !short_description, !description, !slug) return new Response(JSON.stringify({message: "All Feilds are required"}), {status: 403})
 
-    const updatePortfolio = await Ourportfolio.findByIdAndUpdate(_id, {$set:{image, title, short_description, slug}});
+    const updatePortfolio = await Ourportfolio.findByIdAndUpdate(_id, {$set:{image, title, short_description, description, slug}});
     if(!updatePortfolio) return new Response(JSON.stringify({message: "Failed to update Portfolio"}), {status: 403})
 
     return new Response(JSON.stringify({message: "Portfolio updated Successfully!"}), {status: 200})
@@ -56,7 +56,7 @@ const deletePortfolio = async (request) => {
     const id = await request.nextUrl.searchParams.get('id');
 
     const deletedata = await Ourportfolio.findByIdAndDelete(id);
-    if(!deletedata) return new Response(JSON.stringify({message: "Failed to delete portfolio"}), {status: 403})
+    if(!deletedata) return new Response(JSON.stringify({message: "Failed to delete Portfolio"}), {status: 403})
 
     return new Response(JSON.stringify({message: "Portfolio deleted Successfully!"}), {status: 200})
 }
