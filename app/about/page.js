a7ccd8bd72db0a120b466432c08615b453ab0692
InfoTech <infotechcompany85@gmail.com>
2025-01-12 20:17:04 +0500

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,3 @@
-"use client";
 import React from 'react'
 import WhyChooseUs from '../components/WhyChooseUs'
 import Image from 'next/image';
@@ -41,4 +40,4 @@ Let's work together to make your business shine online!</p>
     )
 }
 
-export default page  
+export default page
