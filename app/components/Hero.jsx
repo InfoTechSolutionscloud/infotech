f499608a6a32aea39878ec398100a817826d09e3
InfoTech <infotechcompany85@gmail.com>
2025-01-12 18:55:52 +0500

    Update Hero.jsx

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -19,15 +19,15 @@ const Hero = () => {
           transition={{ duration: 1 }}
           className="relative z-10 text-center max-w-3xl text-white"
         >
-          <h1
-            className="text-5xl lg:text-8xl font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-cyan-600"
-            initial={{ y: "100vh", opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 1 }}
-          >
-            Have an Idea? Make It Digital with{" "}
-            <span className="text-secondary-400">InfoTech!</span>
-          </h1>
+         <h1
+  className="text-5xl lg:text-8xl font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-cyan-600"
+  initial={{ y: "100vh", opacity: 0 }}
+  animate={{ y: 0, opacity: 1 }}
+  transition={{ duration: 1 }}
+>
+  Turn Ideas Into Reality with{" "}
+  <span className="text-secondary-400">InfoTech!</span>
+</h1>
 
           <p
             className="mt-6 text-lg lg:text-xl font-medium text-gray-300 leading-relaxed"
