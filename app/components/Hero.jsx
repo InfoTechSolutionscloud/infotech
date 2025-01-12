b8216d94219e405af7cb40833dfbb5cec03627dc
InfoTech <infotechcompany85@gmail.com>
2025-01-12 19:09:31 +0500

    Update Hero.jsx

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -20,18 +20,13 @@ const Hero = () => {
           className="relative z-10 text-center max-w-3xl text-white"
         >
                   <h1
-            className="text-5xl lg:text-7xl font-bold lato text-white text-center md:text-left w-full"
+            className="text-5xl lg:text-7xl font-bold lato text-center md:text-left w-full  text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-cyan-600"
             initial={{ y: "100vh", opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 1 }}
           >
             Have an Idea? Make It Digital with{" "}
-         <span className="text-secondary-400">
-  <span style={{ color: 'rgb(59 236 246 / 87%)' }}>I</span>
-  nfo
-  <span style={{ color: 'rgb(59 236 246 / 87%)' }}>t</span>
-  ech
-</span>!
+  <span className="text-secondary-400">InfoTech!</span>
           </h1>
 
           <p
