76fae9f685c7d08e203e815478b9319a117b4d9e
InfoTech <infotechcompany85@gmail.com>
2025-01-15 17:28:08 +0500

    Update Blogs.jsx

--- app/components/Blogs.jsx
+++ app/components/Blogs.jsx
@@ -43,7 +43,8 @@ const Blogs = ({qty, title="Our Latest"}) => {
             <Loading />
         )}
 
-        {data && (<section className="py-24 bg-gradient-to-r from-[#f0fdfa] to-[#c7f9f4] min-h-screen w-full px-8 lg:px-20">
+        {data && (<section className="py-24 bg-gradient-to-r from-[#155e75] to-[#2dd4bf] min-h-screen w-full px-8 lg:px-20">
+
 
 
 
