1804c3dd27ee207a7d7350d568fe24efa287401c
InfoTech <infotechcompany85@gmail.com>
2025-01-15 17:06:07 +0500

    Update Blogs.jsx

--- app/components/Blogs.jsx
+++ app/components/Blogs.jsx
@@ -43,7 +43,7 @@ const Blogs = ({qty, title="Our Latest"}) => {
             <Loading />
         )}
 
-        {data && (<section className="py-24 bg-gradient-to-tl from-gray-700/25 via-primary-900 to-gray-700/25">
+        {data && (<section className="py-24  bg-gradient-to-r from-secondary-950 to-secondary-900 min-h-screen w-full px-8 lg:px-20">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <motion.h3
                     className="text-3xl text-center py-2 pb-20 lg:text-4xl font-bold lato text-white"
